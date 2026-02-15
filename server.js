24de120f98a4d6b632998db87460a4f0f6899da7
Sahil Abbas <sa9300432@gmail.com>
2026-02-15 23:49:06 +0530

    fiexd url error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import connectDB from './DBconnect/connectdb.js'
 const app = express()
 app.use(cors(
    {
-      origin: ["http://localhost:5173","https://abbastuitionclasses.netlify.app/"],
+      origin: ["http://localhost:5173","https://abbastuitionclasses.netlify.app"],
       methods: ["POST", "GET", "PUT", "DELETE"],
       credentials: true,
    }
